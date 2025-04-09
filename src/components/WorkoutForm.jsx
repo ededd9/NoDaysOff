0b816726d2fabd1949d7d033121535e793734d06
Edward Hurtado <edhur9@gmail.com>
2025-04-08 23:02:52 -0400

    MVC folders

---
+++ src/components/WorkoutForm.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+
+export default function WorkoutForm() {
+  const [exercises, setExercises] = useState({
+    name: "",
+    weight: 0,
+    reps: 0,
+    sets: 0,
+  });
+  const [workout, setWorkout] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setWorkout([...workout, exercises]);
+    setExercises({ name: "", weight: 0, reps: 0, sets: 0 });
+  };
+  console.log(workout);
+  return (
+    <>
+      <div className="container mt-4">
+        <div
+          className="card p-4"
+          style={{ maxWidth: "500px", margin: "0 auto" }}
+        >
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label className="form-label">Exercise Name</label>
+              <input
+                className="form-control"
+                placeholder="e.g. Bench Press"
+                value={exercises.name}
+                onChange={(e) =>
+                  setExercises((prev) => ({ ...prev, name: e.target.value }))
+                }
+              />
+            </div>
+
+            <div className="mb-3">
+              <label className="form-label">Weight (lbs/kg)</label>
+              <input
+                type="number"
+                className="form-control"
+                placeholder="e.g. 135"
+                value={exercises.weight}
+                onChange={(e) =>
+                  setExercises((prev) => ({ ...prev, weight: e.target.value }))
+                }
+              />
+            </div>
+
+            <div className="row g-3 mb-4">
+              <div className="col-md-6">
+                <label className="form-label">Sets</label>
+                <DropdownButton
+                  id="dropdown-basic-button"
+                  title={exercises.sets}
+                  onSelect={(e) =>
+                    setExercises((prev) => ({ ...prev, sets: parseInt(e) }))
+                  }
+                >
+                  {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
+                    <Dropdown.Item eventKey={num} key={num}>
+                      {num}
+                    </Dropdown.Item>
+                  ))}
+                </DropdownButton>
+              </div>
+
+              <div className="col-md-6">
+                <label className="form-label">Reps</label>
+
+                <DropdownButton
+                  id="dropdown-basic-button"
+                  title={exercises.reps}
+                  onSelect={(e) =>
+                    setExercises((prev) => ({ ...prev, reps: parseInt(e) }))
+                  }
+                >
+                  {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(
+                    (num) => (
+                      <Dropdown.Item eventKey={num} key={num}>
+                        {num}
+                      </Dropdown.Item>
+                    )
+                  )}
+                </DropdownButton>
+              </div>
+            </div>
+
+            <button type="submit" className="btn btn-primary w-100">
+              Save Workout
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,20 @@
+import WorkoutForm from "../components/WorkoutForm";
+import Nav from "react-bootstrap/Nav";
+import { useState } from "react";
+export default function Dashboard() {
+  const [showForm, setShowForm] = useState(false);
+  return (
+    <>
+      <Nav defaultActiveKey="/home" className="flex-row" variant="pills">
+        <Nav.Link href="/home">Dashboard</Nav.Link>
+        <Nav.Link eventKey="link-1">
+          <button onClick={() => setShowForm(!showForm)}>
+            {showForm ? "Add workout" : "Add workout"}
+          </button>
+        </Nav.Link>
+        <Nav.Link eventKey="link-2">Statistics</Nav.Link>
+      </Nav>
+      {showForm && <WorkoutForm />}
+    </>
+  );
+}

0c38e4524e9e3932f6ef91b0bcc957d2b28dc880
Edward Hurtado <edhur9@gmail.com>
2025-05-04 23:44:29 -0400

    Finished the user schemas w exercise/workout schemas

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,67 @@
 import mongoose from "mongoose";
 import argon2 from "argon2";
+
+const exerciseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    enum: [
+      "Tricep Extensions",
+      "Incline Bench Press",
+      "Chest Flys",
+      "Deadlift",
+      "Other",
+    ],
+    index: true,
+  },
+  customName: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return this.name !== "Other" || (v && v.length > 0);
+      },
+    },
+    message: "Custom exercise name",
+  },
+  sets: [
+    {
+      setNumber: {
+        type: Number,
+        required: true,
+        min: 1,
+      },
+      reps: {
+        type: Number,
+        required: true,
+        min: 1,
+      },
+      weight: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+    },
+  ],
+});
+
+const workoutLogSchema = new mongoose.Schema(
+  {
+    date: {
+      type: Date,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    exercises: [exerciseSchema],
+  },
+  { timestamps: true }
+);
 //user schema
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true, minlength: 6, select: false },
+  workoutLog: [workoutLogSchema],
 });
 
 userSchema.pre("save", async function (next) {

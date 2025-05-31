80a6ab9391bd4d660ea8ec361988c87f8358b375
Edward Hurtado <edhur9@gmail.com>
2025-05-31 00:12:20 -0400

    no change

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import AuthContext from "./context/authContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <AuthContext>
     <App />
-  </StrictMode>
+  </AuthContext>
 );

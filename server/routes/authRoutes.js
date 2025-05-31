fc9e0b97340ad67da1e4013a819436050f9abf41
Edward Hurtado <edhur9@gmail.com>
2025-05-31 00:18:36 -0400

    added a refresh route for API route -- using for cookies

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,7 +3,8 @@ import User from "../models/User.js";
 import { es } from "date-fns/locale";
 import jwt from "jsonwebtoken";
 const router = express.Router();
-
+const ACCESS_SECRET = process.env.ACCESS_SECRET;
+const REFRESH_SECRET = process.env.REFRESH_SECRET;
 //SIGNUP API ENDPOINT
 router.post("/signup", async (req, res) => {
   try {
@@ -29,6 +30,7 @@ router.post("/signup", async (req, res) => {
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
+    console.log("Session object:", req.session);
     console.log("Request body:", req.body);
     const { email, password } = req.body;
 
@@ -47,18 +49,42 @@ router.post("/login", async (req, res) => {
     if (!isPasswordValid) {
       return res.status(401).json({ status: "fail", message: "Invalid creds" });
     }
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+
+    const accessToken = jwt.sign({ id: user._id }, ACCESS_SECRET, {
       expiresIn: "1h",
     });
+    const refreshToken = jwt.sign({ id: user._id }, REFRESH_SECRET, {
+      expiresIn: "100d",
+    });
 
+    req.session.refreshToken = refreshToken;
     res.status(200).json({
       status: "success",
-      token,
+      accessToken,
       data: { user: { id: user._id, name: user.name, email: user.email } },
     });
   } catch (err) {
     console.error("Login error:", err);
     res.status(400).json({ status: "fail", message: err.message });
   }
+});
+
+router.post("/refresh", (req, res) => {
+  const token = req.session?.refreshToken;
+
+  if (!token) return res.status(401).json({ message: "No refresh token" });
+
+  jwt.verify(token, REFRESH_SECRET, (err, user) => {
+    if (err)
+      return res
+        .status(403)
+        .json({ message: "Invalid or expired refresh token" });
+
+    const newAccessToken = jwt.sign({ id: user.id }, ACCESS_SECRET, {
+      expiresIn: "1h",
+    });
+
+    return res.json({ accessToken: newAccessToken });
+  });
 });
 export default router;

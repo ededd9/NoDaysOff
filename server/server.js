e5c457a817b0ab826af159b9fabad1f68f23a151
Edward Hurtado <edhur9@gmail.com>
2025-05-31 00:11:56 -0400

    changed terminal outputs

--- server/server.js
+++ server/server.js
@@ -3,17 +3,52 @@ import cors from "cors";
 import connectDB from "./db/connection.js";
 import authRoutes from "./routes/authRoutes.js";
 import User from "./models/User.js";
+import workoutRoutes from "./routes/workoutRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import dotenv from "dotenv";
+import cookieSession from "cookie-session";
+dotenv.config();
 const app = express();
 
 connectDB();
 //Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(
+  cookieSession({
+    name: "bezkoder-session",
+    keys: ["COOKIE_SECRET"], // should use as secret environment variable
+    httpOnly: true,
+  })
+);
 app.use(express.json());
+//Req logging middleware
+app.use((req, res, next) => {
+  console.log("====================================================");
+  console.log("Method:", req.method);
+  console.log("Path:", req.path);
+  console.log("Body:", req.body);
+  console.log("====================================================");
+  next();
+});
 //connect to MONGODB
-app.get("/", (req, res) => req.send("MONGODB CONNECTED"));
-const PORT = process.env.PORT || 5000;
+//line 23?
+app.get("/", (req, res) => {
+  res.status(200).send("Server is running and MongoDb is connected");
+});
+
+const PORT = process.env.PORT || 5050;
+
 //Routes
 app.use("/api/auth", authRoutes);
+
+app.use("/api/workouts", workoutRoutes);
+
+app.use("/api/users", userRoutes);
 //know what port is being used
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

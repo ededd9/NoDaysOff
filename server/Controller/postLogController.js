a552890dcc67d9adb272f032bff773501ad71493
Edward Hurtado <edhur9@gmail.com>
2025-06-17 00:07:30 -0400

    created a post controller where the create post endpoint is located

---
+++ server/Controller/postLogController.js
@@ -0,0 +1,31 @@
+import Post from "../models/Post.js";
+import User from "../models/User.js";
+
+export const createPost = async (req, res) => {
+  try {
+    console.log("Incoming request body:", req.body);
+    const { content } = req.body;
+    const userId = req.user._id;
+    if (!userId || !content) {
+      console.log("Missing required fields");
+      return res.status(400).json({ error: "Missing userId or content" });
+    }
+    const newPost = new Post({
+      user: userId,
+      content,
+    });
+
+    const savedPost = await newPost.save();
+    const populatedPost = await Post.findById(savedPost._id).populate(
+      "user",
+      "name email"
+    );
+    await User.findByIdAndUpdate(userId, {
+      $push: { posts: savedPost._id, postLog: savedPost._id },
+    });
+
+    res.status(201).json(populatedPost);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};

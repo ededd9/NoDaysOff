a67bcce53cac0f0fc492d6ef20ff3849475c83da
Edward Hurtado <edhur9@gmail.com>
2025-07-06 17:18:19 -0400

    commented out CLs

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useContext, createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 export const userContext = createContext();
 
 export default function AuthContext({ children }) {
@@ -8,17 +9,23 @@ export default function AuthContext({ children }) {
   //check if user has already been logged in, if so, then when page refreshes, keep user state logged in
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
-    if (storedUser) {
+    const token = localStorage.getItem("token");
+    if (storedUser && token) {
       setUser(JSON.parse(storedUser));
     }
   }, []);
+  //const navigate = useNavigate();
   //login user state
-  const login = (userData) => {
+  const login = (userData, token) => {
     setUser(userData);
     //storing data info in local storage
     localStorage.setItem("user", JSON.stringify(userData));
+    localStorage.setItem("token", token);
+    // Navigate("/home");
+    // navigate("/home");
   };
   const logout = () => {
+    console.log("User logging out - clearing all data");
     localStorage.removeItem("token");
     localStorage.removeItem("user");
     setUser(null);

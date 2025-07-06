9849e2ac76dbf330a8893b2a457dced50c07d5f6
Edward Hurtado <edhur9@gmail.com>
2025-07-06 17:16:51 -0400

    no change

--- server/Controller/postLogController.js
+++ server/Controller/postLogController.js
@@ -5,7 +5,7 @@ export const createPost = async (req, res) => {
   try {
     console.log("Incoming request body:", req.body);
     const { content } = req.body;
-    const userId = req.user._id;
+    const userId = req.user.id;
     if (!userId || !content) {
       console.log("Missing required fields");
       return res.status(400).json({ error: "Missing userId or content" });
@@ -16,12 +16,12 @@ export const createPost = async (req, res) => {
     });
 
     const savedPost = await newPost.save();
-    const populatedPost = await Post.findById(savedPost._id).populate(
+    const populatedPost = await Post.findById(savedPost.id).populate(
       "user",
       "name email"
     );
     await User.findByIdAndUpdate(userId, {
-      $push: { posts: savedPost._id, postLog: savedPost._id },
+      $push: { posts: savedPost.id, postLog: savedPost.id },
     });
 
     res.status(201).json(populatedPost);

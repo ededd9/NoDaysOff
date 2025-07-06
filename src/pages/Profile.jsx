5da6d06ca631a11df8b940c3b01345378a5c7136
Edward Hurtado <edhur9@gmail.com>
2025-07-06 17:19:07 -0400

    added ability to delete, edit own posts in /Profile, made it so that if not own profile, can view other peoples profiles

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,23 +1,24 @@
 import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useAuth } from "../context/authContext";
+import { Link } from "react-router-dom";
 import axios from "axios";
 export default function Profile() {
-  const { user } = useAuth(); // currently logged in user
+  const { user, login } = useAuth(); // currently logged in user
   const { userId } = useParams(); //user id from url
-
-  console.log(user);
+  const token = localStorage.getItem("token");
   const [editProfile, setEditProfile] = useState(false);
+  const [editingPost, setEditingPost] = useState(false);
+  const [editingPostId, setEditingPostId] = useState(null);
+  const [editedContent, setEditedContent] = useState("");
   const [userPosts, setUserPosts] = useState([]);
   const [profileUser, setProfileUser] = useState(null);
   const [isFollowing, setIsFollowing] = useState(false);
   const [followList, setFollowList] = useState([]);
   //check if user is viewing their own profile
-  const isOwnProfile = !userId || userId === user?.id;
-
+  const isOwnProfile = !userId || userId === user._id;
   const displayUser = isOwnProfile ? user : profileUser;
-  // console.log(user, editProfile);
-
+  const [formData, setFormData] = useState({});
   useEffect(() => {
     const fetchUserPosts = async () => {
       try {
@@ -45,7 +46,7 @@ export default function Profile() {
         //console.log(userPosts);
         //grab the targetted user id posts , whether it be the own user or someone elses
         //already have own users data, so need data of the profile user is viewing
-        const targetUserId = isOwnProfile ? user?.id : userId;
+        const targetUserId = isOwnProfile ? user._id : userId;
         const postResponse = await axios.get(
           `http://localhost:5050/api/posts/user/${targetUserId}`,
           {
@@ -62,23 +63,30 @@ export default function Profile() {
             withCredentials: true,
           }
         );
-
         setFollowList(followingResponse.data);
       } catch (err) {
         console.log("Failed to load posts: ", err);
       }
     };
     //only fetch user/post data if llogged in user exists
-    if (user?.id) {
+    if (user && user._id && token) {
       fetchUserPosts();
     }
   }, [user, userId, isOwnProfile]);
+  useEffect(() => {
+    if (displayUser) {
+      setFormData({
+        name: displayUser.name || "",
+        email: displayUser.email || "",
+        bio: displayUser.bio || "",
+      });
+    }
+  }, [displayUser]);
   //console.log(userPosts);
   const handleFollow = async () => {
     try {
-      console.log("try to follow user:", displayUser._id);
       const token = localStorage.getItem("token");
-      console.log("Using token:", token); // Debug
+
       const response = await axios.put(
         `http://localhost:5050/api/users/${displayUser._id}/follow`,
         {}, // empty data object
@@ -98,7 +106,94 @@ export default function Profile() {
       console.log("Error in following or unfollowing user:", err);
     }
   };
-  console.log("test", followList);
+  const handleDelete = async (id) => {
+    const prevPosts = [...userPosts];
+    try {
+      setUserPosts(userPosts.filter((post) => post._id !== id));
+      await axios.delete(`http://localhost:5050/api/posts/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+        withCredentials: "true",
+      });
+      console.log("Success");
+    } catch (err) {
+      setUserPosts(prevPosts);
+      console.log("Error in deleting post: ", err);
+    }
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleUpdate = async () => {
+    try {
+      const response = await axios.patch(
+        `http://localhost:5050/api/users/${user._id}`,
+        formData,
+        { headers: { Authorization: `Bearer ${token}` }, withCredentials: true }
+      );
+      const updatedUser = response.data;
+      login(updatedUser);
+      setEditProfile(false);
+      if (!isOwnProfile) {
+        setProfileUser(updatedUser);
+      }
+    } catch (err) {
+      console.log("Update failed ", err);
+    }
+  };
+  const handleSaveEdit = async (postId) => {
+    try {
+      const response = await axios.patch(
+        `http://localhost:5050/api/posts/${postId}`,
+        { content: editedContent },
+        { headers: { Authorization: `Bearer ${token}` }, withCredentials: true }
+      );
+
+      setUserPosts((prevPosts) =>
+        prevPosts.map((post) =>
+          post._id === postId ? { ...post, content: editedContent } : post
+        )
+      );
+      setEditingPostId(null);
+      setEditedContent("");
+    } catch (err) {
+      console.log("Error updating post: ", err);
+    }
+  };
+  const handleEdit = (post) => {
+    setEditingPostId(post._id);
+    setEditedContent(post.content);
+  };
+  const handleLike = async (id) => {
+    const token = localStorage.getItem("token");
+    try {
+      //url, data, config
+      const response = await axios.put(
+        `http://localhost:5050/api/posts/${id}/like`,
+        {}, //not sending data so {}
+        {
+          headers: { Authorization: `Bearer ${token}` },
+          withCredentials: "true",
+        }
+      );
+      //if can access like endpoint, check if the current post id matches the
+      //post the user is trying to like , update only the like attribute of the post, if not, then
+      //dont modify the post at all and return it
+      if (response.data.status === "success") {
+        setUserPosts((prevPosts) =>
+          prevPosts.map((post) =>
+            post._id === id
+              ? { ...post, likes: response.data.data.likes }
+              : post
+          )
+        );
+      }
+    } catch (err) {
+      console.log("Error in liking post: ", err);
+    }
+  };
+
   return (
     <div className="pt-20 min-h-screen bg-gray-100">
       {/* Profile Section - Top of page under navbar */}
@@ -109,6 +204,8 @@ export default function Profile() {
               <label className="block text-gray-700 text-sm font-bold mb-2">
                 <input
                   placeholder={displayUser?.name}
+                  onChange={handleChange}
+                  value={formData.name}
                   name="name"
                   className="w-full p-2 border rounded"
                 />
@@ -117,44 +214,37 @@ export default function Profile() {
                 <input
                   placeholder="Email"
                   name="email"
-                  className="w-full p-2 border rounded"
-                />
-              </label>
-              {/* <label className="block text-gray-700 text-sm font-bold mb-2">
-                <input
-                  placeholder="Gender"
-                  name="gender"
+                  onChange={handleChange}
+                  value={formData.email}
                   className="w-full p-2 border rounded"
                 />
               </label>
               <label className="block text-gray-700 text-sm font-bold mb-2">
                 <input
-                  placeholder="Weight"
-                  name="weight"
+                  placeholder="Bio"
+                  name="bio"
+                  onChange={handleChange}
+                  value={formData.bio}
                   className="w-full p-2 border rounded"
                 />
               </label>
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                <input
-                  placeholder="Height"
-                  name="height"
-                  className="w-full p-2 border rounded"
-                />
-              </label> */}
             </>
           ) : isOwnProfile ? (
             <>
               <h2 className="text-2xl font-semibold mb-2">
                 {displayUser?.name}
               </h2>
+              <h3>{displayUser?.bio}</h3>
 
               <p className="text-gray-600 mb-1">Email | {displayUser?.email}</p>
             </>
           ) : (
             <>
               <h2 className="text-2xl font-semibold mb-2">
-                Check out {displayUser?.name}'s posts!
+                {displayUser?.name}'s posts!
               </h2>
+              <h5>{displayUser?.bio ? displayUser?.bio : "No bio"} </h5>
+
               <button
                 onClick={() => handleFollow()}
                 className="text-xs bg-blue-100 text-blue-600 px-2 py-1 rounded hover:bg-blue-200 transition"
@@ -163,6 +253,7 @@ export default function Profile() {
               </button>
             </>
           )}
+
           {isOwnProfile && (
             <div className="mt-4 space-x-2">
               <button
@@ -172,11 +263,41 @@ export default function Profile() {
                 {editProfile ? "Cancel" : "Edit"}
               </button>
               {editProfile && (
-                <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
+                <button
+                  className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+                  onClick={handleUpdate}
+                >
                   Save
                 </button>
               )}
             </div>
+          )}
+        </div>
+        <div className="container mx-auto px-4 pb-8">
+          <h3 className="text-2xl font-bold mb-8 text-center">Following</h3>
+
+          {followList.length > 0 ? (
+            <div className="flex flex-wrap justify-center gap-4">
+              {followList.map((user, i) => (
+                <div
+                  key={i}
+                  className="bg-white p-4 rounded-lg shadow-md text-center w-60"
+                >
+                  <Link
+                    to={`/Profile/${user._id}`}
+                    style={{ textDecoration: "none" }}
+                    className="flex items-center gap-3 no-underline"
+                  >
+                    <div className="w-10 h-10 bg-gray-300 rounded-full mr-3"></div>
+                    <div>
+                      <span className="text-sm font-medium">{user.name}</span>
+                    </div>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p className="text-center">No one yet :)</p>
           )}
         </div>
       </div>
@@ -216,31 +337,60 @@ export default function Profile() {
                 {isOwnProfile && (
                   <div className="flex space-x-1 mb-3">
                     <button
+                      onClick={() => handleLike(post._id)}
+                      className="text-xs bg-green-100 text-green-600 px-1 py-1 rounded hover:bg-green-200 transition"
+                    >
+                      🤍
+                      {post.likes.length}
+                    </button>
+                    <button
                       onClick={() => handleDelete(post._id)}
                       className="text-xs bg-red-100 text-red-600 px-2 py-1 rounded hover:bg-red-200 transition"
                     >
                       Delete
                     </button>
-                    <button className="text-xs bg-blue-100 text-blue-600 px-2 py-1 rounded hover:bg-blue-200 transition">
-                      Edit
-                    </button>
+                    {editingPostId === post._id ? (
+                      <>
+                        <button
+                          onClick={() => handleSaveEdit(post._id)}
+                          className="text-xs bg-green-100 text-green-600 px-1 py-1 rounded hover:bg-green-200 transition"
+                        >
+                          Save
+                        </button>
+                        <button
+                          onClick={() => {
+                            setEditingPostId(null);
+                            setEditedContent("");
+                          }}
+                          className="text-xs bg-gray-100 text-gray-600 px-1 py-1 rounded hover:bg-gray-200 transition"
+                        >
+                          Cancel
+                        </button>
+                      </>
+                    ) : (
+                      <button
+                        onClick={() => handleEdit(post)}
+                        className="text-xs bg-blue-100 text-blue-600 px-2 py-1 rounded hover:bg-blue-200 transition"
+                      >
+                        Edit
+                      </button>
+                    )}
                   </div>
                 )}
-                <p className="text-gray-800 text-sm line-clamp-4">
-                  {post.content}
-                </p>
+                {editingPostId === post._id ? (
+                  <textarea
+                    value={editedContent}
+                    onChange={(e) => setEditedContent(e.target.value)}
+                  />
+                ) : (
+                  <p className="text-gray-800 text-sm line-clamp-4">
+                    {post.content}
+                  </p>
+                )}
               </div>
             ))}
           </div>
         )}
-      </div>
-      <div className="container mx-auto px-4 pb-8">
-        <h3 className="text-2xl font-bold mb-8 text-center">Following</h3>
-        <div className="bg-white p-6 rounded-lg shadow-md text-center max-w-md mx-auto">
-          {followList.map((user, i) => (
-            <ul>{user.name}</ul>
-          ))}
-        </div>
       </div>
     </div>
   );

3811c5e0c47b14389f39c2d0e191b853a7cc52f4
Edward Hurtado <edhur9@gmail.com>
2026-06-04 21:16:54 -0400

    removed unused import, fixed isNew check as old was using mongoose  and unreliable

--- server/Controller/workoutLogController.js
+++ server/Controller/workoutLogController.js
@@ -1,5 +1,3 @@
-// controllers/workoutLogController.js
-import mongoose from "mongoose";
 import Workout from "../models/Workout.js";
 import User from "../models/User.js";
 
@@ -26,11 +24,13 @@ export const addWorkoutLog = async (req, res) => {
     const workout = await Workout.findOneAndUpdate(
       { user: userId, date: new Date(date) },
       { $push: { exercises: exercise } },
-      { new: true, upsert: true }
+      { new: true, upsert: true },
     );
 
     // Only update user ref if new workout
-    if (workout.$isNew) {
+    const isNewWorkout = workout.exercises.length === 1;
+
+    if (isNewWorkout) {
       await User.findByIdAndUpdate(userId, {
         $addToSet: { workoutLog: workout.id },
       });
@@ -42,10 +42,8 @@ export const addWorkoutLog = async (req, res) => {
       workout,
     });
   } catch (error) {
-    console.error("Error:", error);
     res.status(500).json({
       message: "Failed to save workout",
-      error: error.message,
     });
   }
 };
